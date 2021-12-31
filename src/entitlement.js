b2d194cca2acc446ea54b552771679ea6e1d4864
Mik Freedman <github@michael-freedman.com>
2021-12-30 21:24:12 -0500

    fix some JSX errors on class --> className

--- src/entitlement.js
+++ src/entitlement.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { launchURL } from './services/url';
 
 const Entitlement = ({ entitlement }) => {
-    return <tr class="item">
+    return <tr className="item">
         <td><img width="40" src={entitlement._links.icon.href} /></td>
         <td><a href={entitlement._links.launch.href}>{entitlement.name}</a></td>
-        <td class="favorite">
+        <td className="favorite">
             {entitlement.favorite &&
                 <img src="/css/favorite.png" width="20" height="20" />
             }
