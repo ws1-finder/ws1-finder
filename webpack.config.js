db6fe6a9f417ade7ab558aaf11cf42192ff47d31
Mik Freedman <github@michael-freedman.com>
2022-01-02 09:50:40 -0500

    install material ui

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,11 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader']
-      }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ]
   },
   resolve: {
