a0cf50aadde94101f075a5e8c32a421d124b9a28
Mik Freedman <github@michael-freedman.com>
2022-01-02 22:41:07 -0500

    use the service for URL launching

--- src/popup/services/entitlements.js
+++ src/popup/services/entitlements.js
@@ -1,11 +1,10 @@
-import { getBaseURL } from "./url";
+import { getBaseURL, launchURLAndClose } from "./url";
 
 export function getEntitlements(backgroundPage = chrome.extension.getBackgroundPage()) {
     return backgroundPage.getEntitlements()
         .catch(err => {
             getBaseURL().then(url => {
-                chrome.tabs.create({ url: url });
-                window.close()
+                launchURLAndClose(url)
             });
 
             reject(err)
