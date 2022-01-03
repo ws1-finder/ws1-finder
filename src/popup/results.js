ab4701c75083569cbe395a0e2a8f2e3930539cb6
Mik Freedman <github@michael-freedman.com>
2022-01-02 21:59:18 -0500

    show a divider for search results

--- src/popup/results.js
+++ src/popup/results.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
 import Result from './result';
 
 const Results = ({ results }) => {
     return <List>
-        {results.map((result) => (
-            <Result result={result} key={result.key} />
+        {results.map((result, index) => (
+            <div key={result.key}>
+                <Result result={result} />
+                {(index < results.length - 1) ? <Divider /> : null}
+            </div>
         ))}
     </List>
 }
