e89fc3718fa8a1831394cf4f148b330611ed800b
Mik Freedman <github@michael-freedman.com>
2022-01-16 09:37:36 -0500

    make keyboard navigation work

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import SearchIcon from "@mui/icons-material/Search";
 import Alert from "@mui/material/Alert";
 import LinearProgress from "@mui/material/LinearProgress";
 import TextField from "@mui/material/TextField";
-import React, { ChangeEvent, KeyboardEvent, useEffect, useState } from "react";
+import React, { ChangeEvent, KeyboardEvent, useCallback, useEffect, useState } from "react";
 import "./App.css";
 import { SearchUpdated } from "./extension";
 import NoResults from "./no_results";
@@ -10,12 +10,12 @@ import Result from "./result";
 import ResultItem from "./result_item";
 import ResultList from "./result_list";
 import { getEntitlements } from "./services/entitlements";
-import { launchURLAndClose } from "./services/url";
 import useSearch from "./use_search";
 import WorkspaceOneHeader from "./workspace_one_header";
 
 function App() {
     const [query, setQuery] = useState("");
+    const [cursor, setCursor] = useState(-1);
     const { isLoading, data, error } = useSearch(getEntitlements, query);
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -26,11 +26,13 @@ function App() {
         }
     };
 
-    const handleKeyPress = (e: KeyboardEvent<HTMLDivElement>) => {
-        if (e.key === "Enter" && data && data.length > 0) {
-            launchURLAndClose(data[0].target);
+    const handleKeyDown = useCallback((e: KeyboardEvent<HTMLElement>) => {
+        if (e.key === "ArrowUp" && cursor > 0) {
+            setCursor(cursor - 1);
+        } else if (e.key === "ArrowDown" && cursor < (data ?? []).length - 1) {
+            setCursor(cursor + 1);
         }
-    };
+    }, [cursor, data]);
 
     useEffect(() => {
         document.body.addEventListener("searchUpdated", ((event: SearchUpdated) => {
@@ -39,20 +41,21 @@ function App() {
     }, []);
     return (<>
         <WorkspaceOneHeader />
+
         <TextField
             id="outlined-basic"
             variant="standard"
             onChange={ handleChange }
-            onKeyDown={ handleKeyPress }
             placeholder="Search VMware Workspace One"
             fullWidth
             autoFocus
+            onKeyDown={ handleKeyDown }
             InputProps={ {
                 startAdornment: <SearchIcon />
             } }
             sx={ { mt: "1em" } }
         />
-        
+
         { error !== undefined && <Alert severity="error">{ error }</Alert> }
         { isLoading && <LinearProgress /> }
         { !isLoading && (
@@ -60,7 +63,12 @@ function App() {
                 { data && data.length === 0 && <NoResults /> }
                 { (data !== undefined && data.length > 0) && (
                     <ResultList>
-                        { data.map((r: Result) => <ResultItem key={ r.key } result={ r } />) }
+                        { data.map((r: Result, i: number) => <ResultItem
+                            selected={ i === cursor }
+                            key={ r.key }
+                            result={ r }
+                            onKeyDown= { handleKeyDown }
+                        />) }
                     </ResultList>
                 ) }
             </>
--- src/result_item.tsx
+++ src/result_item.tsx
@@ -1,17 +1,29 @@
-import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
-import React from "react";
+import React, { createRef, useEffect } from "react";
 import FavoriteAvatar from "./favorite_avatar";
 import Result from "./result";
 import ResultAvatar from "./result_avatar";
 import { handleLaunchURLAndClose } from "./services/url";
 
-const ResultItem = ({ result }: { result: Result }) => {
-    return <ListItem onClick={ handleLaunchURLAndClose(result.target) } button divider>
+const ResultItem = ({ selected, result, ...otherProps }: { selected: boolean, result: Result,  [x:string]: any }) => {
+    const listItem = createRef<HTMLInputElement>();
+
+    useEffect(() => {
+        if(selected && listItem.current) {
+            listItem.current.focus();
+        }
+    }, [selected, listItem]);
+
+    return <ListItemButton 
+        ref={ listItem } 
+        onClick={ handleLaunchURLAndClose(result.target) } 
+        { ...otherProps } 
+        divider>
         <ResultAvatar iconURL={ result.icon } name={ result.name } />
         <ListItemText primary={ result.name } />
         { result.isFavorite && <FavoriteAvatar /> }
-    </ListItem>;
+    </ListItemButton>;
 };
 
 export default ResultItem;
