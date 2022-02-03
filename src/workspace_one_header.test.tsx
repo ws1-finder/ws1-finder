b7b6e55cb06356330ba13c4d8b0aed74db561a80
Mik Freedman <github@michael-freedman.com>
2022-02-03 07:11:45 -0500

    add test for header

---
+++ src/workspace_one_header.test.tsx
@@ -0,0 +1,21 @@
+import { cleanup, render, screen } from "@testing-library/react";
+import React from "react";
+import "@testing-library/jest-dom";
+import WorkspaceOneHeader from "./workspace_one_header";
+
+jest.mock("./services/browser");
+
+beforeEach(() => {
+});
+
+afterEach(() => {
+    cleanup();
+});
+
+it("renders the item", async () => {
+    render(<WorkspaceOneHeader />);
+    await screen.findByRole("heading");
+  
+    expect(screen.getByRole("link")).toHaveTextContent("Workspace One Finder");
+    expect(screen.getByRole("link")).toHaveAttribute("href", "https://finder.example.com");
+});
