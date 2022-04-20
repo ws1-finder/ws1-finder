6a3f2de090f747680c11e48878bd5ddc331cd6b8
Mik Freedman <github@michael-freedman.com>
2022-04-20 11:45:13 -0400

    update manifest script takes version name suffix

--- scripts/update_manifest_version.js
+++ scripts/update_manifest_version.js
@@ -1,3 +1,5 @@
+const argv = process.argv.slice(2);
+
 const path = require('path/posix');
 const fs = require('fs-extra');
 const paths = require('react-scripts/config/paths');
@@ -8,7 +10,12 @@ let rawManifest = fs.readFileSync(manifestFilename);
 let manifest = JSON.parse(rawManifest);
 
 manifest["version"] = package.version;
-manifest["version_name"] = package.version;
+let versionName = [ package.version ]
 
-console.log(`Updating manifest version to ${package.version}`);
+if(argv[0] && argv[0].length > 0)
+   versionName.push(argv[0]);
+
+manifest["version_name"] = versionName.join('-');
+
+console.log(`Updating manifest version to ${package.version} and version_name to ${manifest["version_name"]}`);
 fs.writeFileSync(manifestFilename, JSON.stringify(manifest, null, 2));
