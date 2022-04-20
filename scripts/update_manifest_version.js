119e94577ae25da371fee8e20aab73a4bea01f1a
Mik Freedman <github@michael-freedman.com>
2022-04-20 11:51:43 -0400

    allow one to update the build manifest instead of the source manifest

--- scripts/update_manifest_version.js
+++ scripts/update_manifest_version.js
@@ -1,10 +1,12 @@
+const UPDATE_BUILT_MANIFEST = "UPDATE_BUILT_MANIFEST";
 const argv = process.argv.slice(2);
 
 const path = require('path/posix');
 const fs = require('fs-extra');
 const paths = require('react-scripts/config/paths');
 const package = require(paths.appPackageJson);
-const manifestFilename = path.join(paths.appPublic, './manifest.json');
+
+const manifestFilename = path.join(process.env[UPDATE_BUILT_MANIFEST] === 'true' ? paths.appBuild : paths.appPublic, './manifest.json');
 
 let rawManifest = fs.readFileSync(manifestFilename);
 let manifest = JSON.parse(rawManifest);
